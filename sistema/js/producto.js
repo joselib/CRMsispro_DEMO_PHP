bfe5bc0db8988a9ec56568c60a3d68ed31818949
joselib <54868005+joselib@users.noreply.github.com>
2021-11-07 15:55:40 -0500

    alerta cierre de ventana

--- sistema/js/producto.js
+++ sistema/js/producto.js
@@ -99,3 +99,24 @@ function validaPass() {
     $('.alertChangePass').html('<p style="color:blue;">Las contraseñas Coinciden.</p>');
     $('.alertChangePass').slideDown();
 }
+//alerta de cierre de ventana window
+
+window.onbeforeunload = function(event) {
+    var message = 'Important: Please click on \'Save\' button to leave this page.';
+    if (typeof event == 'undefined') {
+        event = window.event;
+    }
+    if (event) {
+        event.returnValue = message;
+    }
+    return message;
+};
+
+$(function() {
+    $("a").not('#lnkLogOut').click(function() {
+        window.onbeforeunload = null;
+    });
+    $(".btn").click(function() {
+        window.onbeforeunload = null;
+    });
+});
